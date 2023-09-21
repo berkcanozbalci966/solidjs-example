02267973946edb6bb69bae80657737fc05b2e074
berkcanozbalci966 <berkcanozbalci@gmail.com>
2023-09-21 11:05:42 +0300

    feat: initial & router implementation

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+
+import "./index.css";
+import App from "./App";
+import { Route, Router, Routes } from "@solidjs/router";
+import { TestPage } from "./test";
+
+const root = document.getElementById("root");
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
+  );
+}
+
+render(
+  () => (
+    <Router>
+      <Routes>
+        <Route path="/" component={App} />
+        <Route path="/test" component={TestPage} />
+      </Routes>
+    </Router>
+  ),
+  root
+);
---
+++ src/test.jsx
@@ -0,0 +1,8 @@
+export function TestPage() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <h2>Test2</h2>
+    </div>
+  );
+}
